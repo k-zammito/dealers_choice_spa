bf0f9ae019395c32eadc241ab0b1a3868585699d
k-zammito <kzammito@gmail.com>
2021-11-24 16:39:34 -0500

    added has change links, need to fix descriptions api routing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
 //this file is for the client side 
-
 import axios from 'axios';
+
+const characterList = document.querySelector('#char-list');
+const descriptionList = document.querySelector('#desc-list');
+
+const renderCharacters = (characters) => {
+    const html = characters.map(char => `
+        <li>
+            <a href='#${char.id}'>
+            ${char.name}
+            </a>
+        </li>
+    `).join('');
+    characterList.innerHTML = html;
+}
+
+const renderDescriptions = (descriptions) => {
+    const html = descriptions.map(desc => `
+        <li>
+            <a href='#${desc.id}'>
+            ${desc.name}
+            </a>
+        </li>
+    `).join('');
+    descriptionList.innerHTML = html;
+}
 
 const init = async() => {
     try {
         const characters = (await axios.get('/api/characters')).data;
         const descriptions = (await axios.get('/api/descriptions')).data;
-        console.log(characters)
-        console.log(descriptions)
+        renderCharacters(characters);
+        // renderDescriptions(descriptions);
     }
     catch(ex) {
         console.log(ex)
     }
 }
+
+window.addEventListener('hashchange', async() => {
+    const characterId = window.location.hash.slice(1)
+    // const url = `/api/characters/${characterId}/bio`;
+    // const bios = (await axios(url)).data
+
+    // NEED TO SET UP DESCRIPTIONS LIKE SALES IN VIDEO!
+
+});
 
 init()
